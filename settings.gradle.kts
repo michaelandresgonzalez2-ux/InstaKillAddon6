d1417e55ac9673ff380596ea35aece8b5ff7715b
michaelandresgonzalez2-ux <michaelandresgonzalez2@gmail.com>
2026-07-29 11:27:14 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
         maven("https://maven.meteordev.org/releases")
-        maven("https://maven.meteordev.org/snapshots")
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
-rootProject.name = "addon-template"
+rootProject.name = "InstaKillAddon"
